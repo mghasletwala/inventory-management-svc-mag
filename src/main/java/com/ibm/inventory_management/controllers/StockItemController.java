4124e3e9c4c178061093512fd427159036301d8b
mghasletwala <31267405+mghasletwala@users.noreply.github.com>
2021-08-29 18:44:53 +0530

    Service implemnattion added in controller

--- src/main/java/com/ibm/inventory_management/controllers/StockItemController.java
+++ src/main/java/com/ibm/inventory_management/controllers/StockItemController.java
@@ -1,16 +1,35 @@
    package com.ibm.inventory_management.controllers;
 
+   import com.ibm.inventory_management.models.StockItem;
+   import com.ibm.inventory_management.services.StockItemApi;
+
    import java.util.ArrayList;
    import java.util.List;
 
    import org.springframework.web.bind.annotation.GetMapping;
    import org.springframework.web.bind.annotation.RestController;
+   import org.springframework.context.annotation.Lazy;
 
    @RestController
   public class StockItemController {
 
+    private final StockItemApi service;
+
+   public StockItemController(@Lazy StockItemApi service) {
+        this.service = service;
+    }
+
   @GetMapping(path = "/stock-items", produces = "application/json")
   public List listStockItems() {
-   return new ArrayList();
+
+        List<StockItem> items = new ArrayList<StockItem>();
+        try {
+            items = this.service.listStockItems();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return items;
   }
-  }
+}
