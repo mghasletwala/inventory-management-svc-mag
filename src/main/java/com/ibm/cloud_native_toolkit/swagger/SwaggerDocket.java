6fc425473ed1f9fcb4f98c945d79b4ce04661e0f
mghasletwala <31267405+mghasletwala@users.noreply.github.com>
2021-08-29 18:24:04 +0530

    bug fix

--- src/main/java/com/ibm/cloud_native_toolkit/swagger/SwaggerDocket.java
+++ src/main/java/com/ibm/cloud_native_toolkit/swagger/SwaggerDocket.java
@@ -33,7 +33,7 @@ public class SwaggerDocket {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(buildApiRequestHandler()::test)
-                .paths(PathSelectors.regex(".*stock-item.*")::test)
+                .paths(buildPathSelector())::test)
                 .build()
                 .apiInfo(buildApiInfo());
     }
